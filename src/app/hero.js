56a1f331cb91f2a04e9ff2ff04bcaf071754d3d4
Conor Budge <conorbudge555@gmail.com>
2017-02-06 15:58:10 -0600

    Initial commit, not including node_modules

---
+++ src/app/hero.js
@@ -0,0 +1,13 @@
+"use strict";
+/**
+ * Created by conor.budge on 2/6/2017.
+ */
+var Hero = (function () {
+    function Hero(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+//# sourceMappingURL=hero.js.map
---
+++ src/app/hero.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by conor.budge on 2/6/2017.
+ */
+export class Hero {
+  constructor(
+    public id: number,
+    public name: string) {}
+}
