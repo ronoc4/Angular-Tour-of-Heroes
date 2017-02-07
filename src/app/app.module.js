ec2dd9eb41c0a564f1d695da5b6095383b774f69
conor.budge <conor.budge@concordusa.com>
2017-02-07 16:19:32 -0600

    Making initial page, and further modulating the app

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -11,15 +11,18 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
 var forms_1 = require('@angular/forms');
-var app_component_1 = require('./app.component');
+var hero_component_1 = require('./hero.component');
 var hero_detail_component_1 = require('./hero-detail.component');
+var app_component_1 = require("./app.component");
+var hero_service_1 = require("./hero.service");
 var AppModule = (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
             imports: [platform_browser_1.BrowserModule, forms_1.FormsModule],
-            declarations: [app_component_1.AppComponent, hero_detail_component_1.HeroDetailComponent],
+            declarations: [app_component_1.AppComponent, hero_detail_component_1.HeroDetailComponent, hero_component_1.HeroesComponent],
+            providers: [hero_service_1.HeroService],
             bootstrap: [app_component_1.AppComponent]
         }), 
         __metadata('design:paramtypes', [])
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent }  from './app.component';
+import { HeroesComponent }  from './hero.component';
 import  { HeroDetailComponent } from './hero-detail.component';
+import {AppComponent} from "./app.component";
+import {HeroService} from "./hero.service";
 
 @NgModule({
   imports:      [ BrowserModule , FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent ],
+  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent ],
+  providers:    [ HeroService ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
