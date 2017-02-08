fad509614bd8d5867006c34524085a589095b375
conor.budge <conor.budge@concordusa.com>
2017-02-08 12:51:52 -0600

    Added more styles, routing, and services

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -14,11 +14,12 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = 'Tour of Heroes';
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n    <h1>{{title}}</h1>\n    <my-heroes></my-heroes>\n",
+            template: "\n    <h1>{{title}}</h1>\n    <nav>\n      <a routerLink=\"/dashboard\">Dashboard</a>\n      <a routerLink=\"/heroes\">Heroes</a>\n    </nav>\n    <router-outlet></router-outlet>\n",
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- src/app/app.module.js
+++ src/app/app.module.js
@@ -11,17 +11,28 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
 var forms_1 = require('@angular/forms');
-var hero_component_1 = require('./hero.component');
+var app_component_1 = require('./app.component');
+var dashboard_component_1 = require('./dashboard.component');
 var hero_detail_component_1 = require('./hero-detail.component');
-var app_component_1 = require("./app.component");
-var hero_service_1 = require("./hero.service");
+var heroes_component_1 = require('./heroes.component');
+var hero_service_1 = require('./hero.service');
+var app_routing_module_1 = require('./app-routing.module');
 var AppModule = (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule, forms_1.FormsModule],
-            declarations: [app_component_1.AppComponent, hero_detail_component_1.HeroDetailComponent, hero_component_1.HeroesComponent],
+            imports: [
+                platform_browser_1.BrowserModule,
+                forms_1.FormsModule,
+                app_routing_module_1.AppRoutingModule
+            ],
+            declarations: [
+                app_component_1.AppComponent,
+                dashboard_component_1.DashboardComponent,
+                hero_detail_component_1.HeroDetailComponent,
+                heroes_component_1.HeroesComponent
+            ],
             providers: [hero_service_1.HeroService],
             bootstrap: [app_component_1.AppComponent]
         }), 
